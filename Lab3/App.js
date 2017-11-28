edb854f42e749e932d1cc7855322e0db01895187
ZouDikai <zdk_cz@sina.com>
2017-11-28 21:57:35 +0800

    Add ears

--- Lab3/App.js
+++ Lab3/App.js
@@ -122,7 +122,7 @@ function App(ch, lh, dh) {
         gl.uniform4fv(uColor, object.color);
       }
 
-      gl.disableVertexAttribArray(aVertexColor);
+      // gl.disableVertexAttribArray(aVertexColor);
 
       gl.bindBuffer(gl.ARRAY_BUFFER, object.vbo);
       gl.vertexAttribPointer(aVertexPosition, 3, gl.FLOAT, false, 0, 0);
