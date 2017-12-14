8c99275965a8e5a11e13931b20d0c3c0fd4bc3a5
Dokyme <zdkscope@qq.com>
2017-12-15 01:28:09 +0800

    Mkdir lab5

---
+++ Lab5/Light.js
@@ -0,0 +1,6 @@
+var Light = {
+  lightPosition: vec4(1.0, 1.0, 1.0, 1.0),
+  lightAmbient: vec4(1.0, 1.0, 1.0, 1.0),
+  lightDiffuse: vec4(1.0, 1.0, 1.0, 1.0),
+  lightSpecular: vec4(0.5, 0.5, 0.5, 1.0)
+};
---
+++ Lab5/Main.js
@@ -0,0 +1,38 @@
+var gl = null;
+var app = null;
+var modelViewMatrix = mat4();
+var perspectiveMatrix = mat4();
+var program = null;
+let camera = null;
+var interactor = null;
+var viewportWidth = 0;
+var viewportHeight = 0;
+var scene = null;
+
+var uMVMatrix = null;
+var uWireframe = null;
+var uTMatrix = null;
+var uPMatrix = null;
+var aVertexColor = null;
+var aVertexPosition = null;
+var uPerVertexColor = null;
+var uColor = null;
+var uUseTexture = null;
+var uSampler = null;
+var aVertexTextureCoords = null;
+
+//光照有关参数
+var aVertexNormal = null;
+var uNMatrix = null;
+var uLightPosition = null;
+var uAmbientProduct = null;
+var uDiffuseProduct = null;
+var uSpecularProduct = null;
+var uShininess = null;
+
+window.onload = main;
+
+function main() {
+  app = new App();
+  app.run();
+}
